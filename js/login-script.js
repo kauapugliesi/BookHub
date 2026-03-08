f35e77ba1c5b920da3719ea324e948792d07730e
kauapugliesi <pugliesikaua@gmail.com>
2026-03-08 20:42:42 -0300

    Estruturando tela de login e cadastro de usuários, implementando Js para validar

---
+++ js/login-script.js
@@ -0,0 +1,38 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const resultMessage = document.querySelector("#result-message");
+const registerForm = document.querySelector("#register-form");
+
+const validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+};
+
+const validatePassword = (password) => {
+    const regex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(password);
+};
+
+
+registerForm.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
+
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+    const isValidEmail = validateEmail(emailValue);
+    const isValidPassword = validatePassword(passwordValue);
+
+    if (!isValidEmail){
+        resultMessage.textContent = "Email inválido !";
+        resultMessage.style.color = "red"
+    }else if (!isValidPassword) {
+        resultMessage.textContent = "Senha inválida !";
+        resultMessage.style.color = "red"
+
+    }else{
+        registerForm.submit();
+    }
+
+});
